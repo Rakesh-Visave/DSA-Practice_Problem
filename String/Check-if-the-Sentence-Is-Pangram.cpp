892a2584ab467a82ed4eb170dbba875aced06995
Rakesh Visave <visaverakesh20@gmail.com>
2023-07-03 11:01:38 +0530

    Codes

---
+++ String/Check-if-the-Sentence-Is-Pangram.cpp
@@ -0,0 +1,35 @@
+// LEETCODE  -- Check if the Sentence Is Pangram
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+    bool checkIfPangram(string sentence) {
+
+        int arr[26] = {0};
+        for(int i = 0; i<sentence.size(); i++){
+            char ch = sentence[i];
+            int number =  ch - 'a';
+            arr[number]++;
+        }    
+            
+        for(int i = 0; i<26; i++)
+        {
+            if(arr[i] == 0)
+                return false;
+        }
+        return true;
+    }
+
+
+int main()
+{
+    string sentence = "thequickbrownfoxjumpsoverthelazydog";
+
+    bool ans = checkIfPangram(sentence);
+
+    if(ans)
+        cout<<"The string is pangram";
+    else    
+        cout<<"The string is not pangram";
+}
