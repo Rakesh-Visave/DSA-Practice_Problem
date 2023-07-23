66c96785f941ad50252da15420adbb3468ed92e6
Rakesh Visave <visaverakesh20@gmail.com>
2023-07-23 21:57:31 +0530

    Codes

---
+++ Dynamic-Programming/fibonacci-memoization.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fib(int n, vector<int> &dp)
+{
+        // Base Case
+        if(n ==1 || n==0)
+                return n;
+
+        // if solution is calculated and get  the solution from 1-D array and return        
+        if(dp[n] != -1)
+                return dp[n];
+        
+        // storing the result of subproblems
+        dp[n] = fib(n-1,dp) + fib(n-2,dp);
+        return dp[n];
+}
+
+int main()
+{
+        int n ;
+        cin>>n;
+
+        // creat a 1-D array
+        vector<int> dp(n+1);
+        for(int i = 0; i<=n; i++)
+                dp[i] = -1;
+
+        // calling fib Function
+        cout<<fib(n,dp)<<endl;
+        return 0;
+}
