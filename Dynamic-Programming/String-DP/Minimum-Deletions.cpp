5db868fa4f7ef3d2201121848c400b35c55710d0
Rakesh Visave <visaverakesh20@gmail.com>
2023-10-27 20:37:06 +0530

    Codes

---
+++ Dynamic-Programming/String-DP/Minimum-Deletions.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+      int lcs(string &S, string &revS, int i, int j, vector<vector<int> > &dp) {
+        if (i == -1 || j == -1)
+            return 0;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        if (S[i] == revS[j])
+            return dp[i][j] = 1 + lcs(S, revS, i - 1, j - 1, dp);
+
+        int checkS = lcs(S, revS, i - 1, j, dp);
+        int checkRevS = lcs(S, revS, i, j - 1, dp);
+        return dp[i][j] = max(checkS, checkRevS);
+    }
+    int minimumNumberOfDeletions(string S) { 
+         int n = S.size();
+        string revS = S;
+        reverse(revS.begin(), revS.end());
+        vector<vector<int> > dp(n, vector<int>(n, -1));
+
+        return n - lcs(S, revS, n - 1, n - 1, dp);
+    } 
+
+
+int main()
+{
+    string s = "aebcbda";
+
+    int ans = minimumNumberOfDeletions(s);
+
+    cout<<"minimum number of characters from the string so that the resultant string is a palindrome:"<<ans;
+}
